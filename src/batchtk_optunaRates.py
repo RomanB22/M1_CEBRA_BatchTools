21312678dbbbef1e97f86ca1cb74707537c18c21
Roman Baravalle <44242643+RomanB22@users.noreply.github.com>
2025-03-21 08:10:28 -0500

    Update batchtk_optunaRates.py

--- src/batchtk_optunaRates.py
+++ src/batchtk_optunaRates.py
@@ -14,22 +14,17 @@ params = {'weightLong.TPO': [0.25, 0.75],
           'IIweights.1': [0.5, 1.5],
           'IIweights.2': [0.5, 1.5],
           }
-
-# use batch_shell_config if running directly on the machine
-shell_config = {'command': 'python -u init.py'}
-
-# use batch_sge_config if running on a
+# SGE CONFIG
 sge_config = {
     'queue': 'cpu.q',
-    'cores': 5,
-    'vmem': '4G',
-    'realtime': '00:30:00',
+    'cores': 19,
+    'vmem': '90G', #90G
+    'realtime': '15:00:00',
     'command': 'mpiexec -n $NSLOTS -hosts $(hostname) nrniv -python -mpi init.py'}
 
+run_config = sge_config
 
-run_config = shell_config
-
-search(job_type = 'sh', # or sh
+search(job_type = 'sge', # or sh
        comm_type = 'socket',
        label = 'optuna',
        params = params,
@@ -41,3 +36,40 @@ search(job_type = 'sh', # or sh
        mode = 'min',
        algorithm = 'optuna',
        max_concurrent = 1)
+
+
+# EXPANSE CONFIG
+setup = """
+source ~/.bashrc
+source ~/default.sh
+source ~/conda.sh
+conda activate netpyne
+"""
+slurm_config = {
+    'allocation': 'TG-MED240058',
+    'realtime': '10:30:00',
+    'nodes': 1,
+    'coresPerNode': 96,
+    'mem': '128G',
+    'partition': 'compute',
+    'email': 'romanbaravalle@gmail.com',
+    'custom': setup,
+    'command':'time mpirun -n 96 nrniv -python -mpi init.py'
+}
+
+results = search(job_type = 'slurm', # or 'sh'
+       comm_type = 'ssh', # if a metric and mode is specified, some method of communicating with the host needs to be defined
+       label = 'optuna',
+       params = params,
+       output_path = './grid_batch',
+       checkpoint_path = './ray',
+       run_config = slurm_config,
+       metric = 'loss', # if a metric and mode is specified, the search will collect metric data and report on the optimal configuration
+       mode = 'min',
+       algorithm = "optuna",
+       max_concurrent = 3,
+       remote_dir='/home/jchen12/dev/CA3',
+       host='grid0',
+       key='###',
+       num_samples=200,
+       )
